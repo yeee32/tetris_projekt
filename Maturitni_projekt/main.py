a23111151a7394cb99575add4168b07be524be2a
yeee32 <97116766+yeee32@users.noreply.github.com>
2024-03-03 13:57:02 +0100

    Update main.py

--- Maturitni_projekt/main.py
+++ Maturitni_projekt/main.py
@@ -1,5 +1,6 @@
 from setting import *
 import sys
+import random  
 
 # components
 from game import Game
@@ -14,11 +15,24 @@ class Main():
         self.clock = pygame.time.Clock()
         pygame.display.set_caption("TETRIS GAME")
         
+        # next shapes in preview tab
+        self.next_shapes = [random.choice(list(PIECES.keys())) for shape in range(3)]
+        print(self.next_shapes)
         # components 
-        self.game = Game()
+        self.game = Game(self.get_next_shape, self.update_score)
         self.score = Score()
         self.preview = Preview()
-        
+    
+    def update_score(self, lines, curr_score, level):
+        self.score.lines = lines
+        self.score.curr_score = curr_score
+        self.score.level = level     
+
+    def get_next_shape(self):
+        next_shape = self.next_shapes.pop(0)
+        self.next_shapes.append(random.choice(list(PIECES.keys())))
+        return next_shape
+    
     def run(self):
         while RUNNING:
             for event in pygame.event.get():
